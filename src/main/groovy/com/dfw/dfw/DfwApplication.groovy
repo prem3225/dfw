155b90cf955e2e2b32ecb52674e2460b01ba8ea9
Prem kiran <s530481@nwmissouri.edu>
2020-02-04 23:25:05 -0600

    Add dfw project

---
+++ src/main/groovy/com/dfw/dfw/DfwApplication.groovy
@@ -0,0 +1,13 @@
+package com.dfw.dfw
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DfwApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DfwApplication, args)
+	}
+
+}
