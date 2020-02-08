e89e81cebf7a1a80acc479de8089e1b45cdee2db
Prem kiran <premkiran3225@gmail.com>
2020-02-08 11:54:24 -0600

    Update save API functionality

--- src/main/groovy/com/dfw/dfw/entities/UserRepository.groovy
+++ src/main/groovy/com/dfw/dfw/entities/UserRepository.groovy
@@ -1,9 +1,16 @@
 package com.dfw.dfw.entities
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 	
+	@Query("SELECT COUNT(u) FROM User u WHERE u.emailAddress = :email")
+	def findByEmail(@Param("email") String email )
+	
+	@Query("SELECT COUNT(u) FROM User u WHERE u.phoneNumber = :phone")
+	def findByPhoneNumber(@Param("phone") String phone )
 }
--- src/main/groovy/com/dfw/dfw/service/DfwService.groovy
+++ src/main/groovy/com/dfw/dfw/service/DfwService.groovy
@@ -13,7 +13,22 @@ class DfwService {
 	UserRepository userRepository
 	
 	def saveUser(User user) {
-		return userRepository.save(user)
+		int emailCount = userRepository.findByEmail(user.emailAddress)
+		int phoneCount = userRepository.findByPhoneNumber(user.phoneNumber)
+		if(emailCount>0 && phoneCount==0) {
+			return ["result" : "Email Already Exist"]
+		}
+		
+		if(phoneCount>0 && emailCount==0) {
+			return ["result" : "Phone Number Already Exist"]
+		}
+		if(emailCount>0 && phoneCount>0) {
+			return ["result" : "Email address and phone number already exist"]
+		}
+		if(emailCount==0 && phoneCount==0){
+			userRepository.save(user)
+			return ["result" : "Successfully REgistered"]
+		}
 	}
 	
 	def getAllUsers() {
